00ee2d7e987d3cb89aa4eff95cbe4f1ded677213
MartinL3dl <60381918+MartinL3dl@users.noreply.github.com>
2020-11-04 09:50:08 +0100

    Update databaze14.10.sql

--- databaze14.10.sql
+++ databaze14.10.sql
@@ -12,8 +12,16 @@ create table garages(
 id_garage int not null auto_increment,
 email varchar(255),
 telephone int,
+capacity varchar(255),
+GPS varchar(255),
+name varchar(255),
+description varchar(255),
+primary key (id_garage)
 );
-ALTER TABLE garage MODIFY COLUMN id_garage INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+insert into garages
+values (null, 'garaz@seznam.cz', '123456789', '2,265 krychlových metru', '12N, W30. 15 10.234, 30-23.456', 'sklad', 'velkej dum do kteryho skladujes veci :D');
+
 
 create table car (
 id_car int,
@@ -27,6 +35,10 @@ insert into employees values (null,'john', 'smith', '12', 'seznam@.cz');
 insert into garages values (null, 'seznam@.cz', '1234567890');
 insert into car values (null, 'automobilka', 'automobil'); 
 
+/*
 https://i.imgur.com/FSKUidJ.png
 https://i.imgur.com/SlvyOzb.png
 https://i.imgur.com/aSSReIC.png
+https://i.imgur.com/k5AjrSd.png
+https://i.imgur.com/m3xCjGo.png
+*/
